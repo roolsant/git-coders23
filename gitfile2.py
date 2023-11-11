cd323b2ad79f24f8585e6dcc4e9b2620ad441808
Rodrigo Santos <rodrigosantos2007@gmail.com>
2023-11-11 11:15:55 -0300

    inseridas alterações

--- gitfile2.py
+++ gitfile2.py
@@ -2,6 +2,10 @@
 # 11.11.23 - 10:25
 print('Original')
 
-# Arquivo Modificado
+# Arquivo Modificado - 1.0[Teste]
 # 11.11.23 - 10:56
 print('Modificação 1')
+
+# Arquivo Modificado
+# 11.11.23 - 11:01
+print('Modificação 2')
